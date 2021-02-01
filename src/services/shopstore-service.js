d5b91ce2fe5282e7f0efc485124ee7870725918b
Fiodor Furick <fff24.06.91@gmail.com>
2021-02-01 14:19:23 +0200

    add fonts and data in service

--- src/services/shopstore-service.js
+++ src/services/shopstore-service.js
@@ -1,5 +1,21 @@
 export default class ShopstoreService {
 getItems () {
-    return [];
+    return [
+        {
+            itemName: 'CASHES VALLEY LANE',
+            price: 500000,
+            dateOfChange: '01.11.18'
+        },
+        {
+            itemName: 'DURUN DURUN HOUSE',
+            price: 1216000,
+            dateOfChange: '01.11.18'
+        },
+        {
+            itemName: 'CASHES VALLEY LANE',
+            price: 500000,
+            dateOfChange: '01.11.18'
+        }
+    ];
 }
 }
