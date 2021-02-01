9557b513653999478369513adb513c3bf671bdac
Fiodor Furick <fff24.06.91@gmail.com>
2021-02-01 15:56:44 +0200

    create structure of project

--- src/services/shopstore-service.js
+++ src/services/shopstore-service.js
@@ -2,16 +2,19 @@ export default class ShopstoreService {
 getItems () {
     return [
         {
+            id: 1,
             itemName: 'CASHES VALLEY LANE',
             price: 500000,
             dateOfChange: '01.11.18'
         },
         {
+            id: 2,
             itemName: 'DURUN DURUN HOUSE',
             price: 1216000,
             dateOfChange: '01.11.18'
         },
         {
+            id: 3,
             itemName: 'CASHES VALLEY LANE',
             price: 500000,
             dateOfChange: '01.11.18'
