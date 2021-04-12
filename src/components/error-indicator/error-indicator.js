3a88a1f63916c34a9abdc272b3c0e4cfa7b61f48
Fiodor Furick <fff24.06.91@gmail.com>
2021-04-13 02:50:06 +0300

    refactor error-indicator.js

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+
+import './error-indicator.scss';
 
 const ErrorIndicator = () => {
     return (
-        <div>Error!</div>
+        <div className={'error-indicator'}>
+            <div className={'error-indicator-wrap'}>
+                <div className={'error-indicator-big-text'}>
+                    Error!
+                </div>
+                <div className={'error-indicator-big-text'}>
+                    Страница не найдена
+                </div>
+                <div className={'error-indicator-low-text'}>
+                    Страница устарела, была удалена или не существовала вовсе
+                </div>
+                <div className={'back-to-home'}>
+                    <Link to={'/'}>
+                        Вернуться на главную
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
