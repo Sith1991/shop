3fb0deba7ffd6ca825f1e9afa5d12eb72af1c260
Fiodor Furick <fff24.06.91@gmail.com>
2021-06-23 18:50:10 +0300

    start use HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import store from '../src/store/store';
 import ErrorBoundry from './components/error-boundry';
 import App from './components/app';
@@ -11,10 +11,10 @@ import NotificationsContainer from "./components/notifications/notifications-Con
 ReactDOM.render(
     <Provider store={store}> {/*Предоставляет доступ к Redux Store*/}
         <ErrorBoundry> {/*Обработка ошибок в компонентах ниже*/}
-                <Router>
+                <HashRouter>
                     <NotificationsContainer />
                     <App/>
-                </Router>
+                </HashRouter>
         </ErrorBoundry>
     </Provider>,
     document.getElementById('root')
